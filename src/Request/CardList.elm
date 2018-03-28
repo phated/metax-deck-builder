f65faf7afffea2078105a67b09a73378016b1858
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-03-28 15:47:51 -0700

    effectOld

--- src/Request/CardList.elm
+++ src/Request/CardList.elm
@@ -1,45 +1,47 @@
 module Request.CardList exposing (load)
 
 -- import Http
-import Data.CardList as CardList exposing (CardList)
 
+import Data.CardList as CardList exposing (CardList)
 import GraphQl as Gql exposing (Operation, Query, Named)
 import Json.Decode exposing (Decoder, field, string, map2, at, list)
 
+
 cardsQuery : Operation Query Named
 cardsQuery =
-  Gql.named "AllCards" [
-    Gql.field "allCards"
-      |> Gql.withSelectors [
-        Gql.field "uid",
-        Gql.field "set",
-        Gql.field "number",
-        Gql.field "rarity",
-        Gql.field "mp",
-        Gql.field "type",
-        Gql.field "title",
-        Gql.field "subtitle",
-        Gql.field "trait"
-          |> Gql.withSelectors [
-            Gql.field "name"
-          ],
-        Gql.field "effect",
-        Gql.field "stats"
-          |> Gql.withSelectors [
-            Gql.field "type",
-            Gql.field "rank"
-          ],
-        Gql.field "imageUrl",
-        Gql.field "preview"
-          |> Gql.withSelectors [
-              Gql.field "previewer",
-              Gql.field "previewUrl"
-          ]
-      ]
-  ]
+    Gql.named "AllCards"
+        [ Gql.field "allCards"
+            |> Gql.withSelectors
+                [ Gql.field "uid"
+                , Gql.field "set"
+                , Gql.field "number"
+                , Gql.field "rarity"
+                , Gql.field "mp"
+                , Gql.field "type"
+                , Gql.field "title"
+                , Gql.field "subtitle"
+                , Gql.field "trait"
+                    |> Gql.withSelectors
+                        [ Gql.field "name"
+                        ]
+                , Gql.field "effectOld"
+                , Gql.field "stats"
+                    |> Gql.withSelectors
+                        [ Gql.field "type"
+                        , Gql.field "rank"
+                        ]
+                , Gql.field "imageUrl"
+                , Gql.field "preview"
+                    |> Gql.withSelectors
+                        [ Gql.field "previewer"
+                        , Gql.field "previewUrl"
+                        ]
+                ]
+        ]
 
 
-load = Gql.query
-  "https://api.graph.cool/simple/v1/cjerpcdas51ih01414psrg6wa"
-  cardsQuery
-  (at ["allCards"] CardList.decoder)
+load =
+    Gql.query
+        "https://api.graph.cool/simple/v1/cjerpcdas51ih01414psrg6wa"
+        cardsQuery
+        (at [ "allCards" ] CardList.decoder)
